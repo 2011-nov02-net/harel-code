a6592a99a4a50d5c900723447f8d2e0d5f19f3a5
Har'el Fishbein <harelfishbein@users.noreply.github.com>
2020-11-16 11:03:15 -0500

    Update exercise.sql

--- week3day1/exercise.sql
+++ week3day1/exercise.sql
@@ -6,7 +6,7 @@ CREATE TABLE w3.Customers
     ID INT NOT NULL PRIMARY KEY, 
     firstname NVARCHAR(50) NOT NULL,
     lastname NVARCHAR(50) NOT NULL,
-    cardnumber int null CHECK (cardnumber > 99999999999999 and cardnumber < 100000000000000000)
+    cardnumber bigint null
 );
 GO
 
